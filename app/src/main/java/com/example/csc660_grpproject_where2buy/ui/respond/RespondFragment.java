e88f34c8a64ceded3169411593259d3db4fad642
Haz <haziqaiman2@gmail.com>
2022-05-18 02:28:40 +0800

    requests json retrieval

--- app/src/main/java/com/example/csc660_grpproject_where2buy/ui/respond/RespondFragment.java
+++ app/src/main/java/com/example/csc660_grpproject_where2buy/ui/respond/RespondFragment.java
@@ -1,20 +1,50 @@
 package com.example.csc660_grpproject_where2buy.ui.respond;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.csc660_grpproject_where2buy.databinding.FragmentRespondBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RespondFragment extends Fragment {
 
     private FragmentRespondBinding binding;
+    LatLng currentLatLng;
+    FusedLocationProviderClient client;
+    TextView textView;
+    Button refreshBtn;
+    final String URL = "http://www.csc660.ml/getRequestDemo.php";
+    String requesterName, itemName, areaCenterString, requestDateString, statusCode, statusMessage;
+    int requestID;
+
+    RequestQueue queue;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +54,121 @@ public class RespondFragment extends Fragment {
         binding = FragmentRespondBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        respondViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //final TextView textView = binding.textNotifications;
+        //respondViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+        textView = binding.textView;
+        refreshBtn = binding.refreshBtn;
+
+
+
+        client = LocationServices.getFusedLocationProviderClient(getContext());
+
+
+        queue = Volley.newRequestQueue(getContext());
+        refreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getCurrentLocation();
+            }
+        });
+        getCurrentLocation();
         return root;
+    }
+
+    private void sendRequest(){
+        Log.e("RESPONSE", "sendRequest: entered");
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                textView.setText(response);
+                Log.e("RESPONSE", response);
+
+                try{
+                    JSONObject jsonObject = new JSONObject(response);
+                    statusCode = jsonObject.getString("statusCode");
+                    statusMessage = jsonObject.getString("statusMessage");
+
+
+                    if(statusCode.equals("600")){ // if query is successful
+                        JSONArray rows = new JSONArray(response);
+                        JSONObject row;
+
+                        // retrieve data from json
+                        for(int i = 0; i < rows.length(); i++){
+                            row = rows.getJSONObject(i);
+                            requestID = row.getInt("requestID");
+                            requesterName = row.getString("requesterName");
+                            itemName = row.getString("itemName");
+                            areaCenterString = row.getString("areaCenter");
+                            requestDateString = row.getString("requestDate");
+                        }
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, errorListener);
+
+        if(stringRequest != null)
+            queue.add(stringRequest);
+        else
+            Log.e("RESPONSE", "stringRequest is null");
+    }
+
+    public Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.e("RESPONSE", "onErrorResponse: " + error.getMessage());
+        }
+    };
+
+    private void getCurrentLocation(){
+        if(permissionGranted()) {
+            //initialize task location
+            Task<Location> task = client.getLastLocation();
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(final Location location) {
+                    if (location != null) {
+                        //textView.setText("location is " + location.getLatitude() + ", " + location.getLongitude());
+
+                        currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                        //sync map
+                    /*supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            //initialize lat lng
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            //create marker options
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("I am here");
+                            //zoom map scale 15
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                            googleMap.addMarker(options);
+                        }
+                    });*/
+                    } else {
+                        textView.setText("Unable to retrieve location.\n\nTurn on location services or try again later.");
+                    }
+                }
+            });
+            sendRequest();
+        }else{
+            textView.setText("To respond to other people, we need to access your location.\n\nPlease allow the permission.");
+            getPermission();
+        }
+    }
+
+    private boolean permissionGranted(){
+        return ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void getPermission(){
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
     }
 
     @Override
