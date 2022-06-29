bd187edd989fcc9bf1e7e7131fe43d0c9ad70cae
Rahmat Mustafa <66180950+mrahmatm@users.noreply.github.com>
2022-06-29 08:22:32 +0800

    map thingy but api key prob

--- app/src/main/java/com/example/csc660_grpproject_where2buy/ui/map/MapFragment.java
+++ app/src/main/java/com/example/csc660_grpproject_where2buy/ui/map/MapFragment.java
@@ -1,5 +1,7 @@
 package com.example.csc660_grpproject_where2buy.ui.map;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,19 +9,27 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.csc660_grpproject_where2buy.MainActivity;
+import com.example.csc660_grpproject_where2buy.R;
 import com.example.csc660_grpproject_where2buy.databinding.FragmentMapBinding;
+import com.example.csc660_grpproject_where2buy.map.MapViewModel;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private static final String MAPVIEW_BUNDLE_KEY = "AIzaSyCy7mWzBosyA5FFiDb39J7c4Orp0SatpEM";
     private FragmentMapBinding binding;
     String userName, userID;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        MapViewModel mapViewModel =
+        com.example.csc660_grpproject_where2buy.map.MapViewModel mapViewModel =
                 new ViewModelProvider(this).get(MapViewModel.class);
 
         binding = FragmentMapBinding.inflate(inflater, container, false);
@@ -32,12 +42,144 @@ public class MapFragment extends Fragment {
         userID = MainActivity.getUserId();
 
         textView.setText("Hello, " + userName);
-        return root;
+
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        //mUserListRecyclerView = view.findViewById(R.id.user_list_recycler_view);
+        mMapView = view.findViewById(R.id.user_list_map);
+
+        initUserListRecyclerView();
+        initGoogleMap(savedInstanceState);
+
+        return view;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private static final String TAG = "UserListFragment";
+
+    //widgets
+    //private RecyclerView mUserListRecyclerView;
+    private MapView mMapView;
+
+
+    //vars
+    //private ArrayList<User> mUserList = new ArrayList<>();
+    //private UserRecyclerAdapter mUserRecyclerAdapter;
+
+
+    /*public static UserListFragment newInstance() {
+       // return new UserListFragment();
+    }*/
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            //mUserList = getArguments().getParcelableArrayList(getString(R.string.intent_user_list));
+        }
+    }
+
+    @Nullable
+   /* @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        //mUserListRecyclerView = view.findViewById(R.id.user_list_recycler_view);
+        mMapView = view.findViewById(R.id.user_list_map);
+
+        initUserListRecyclerView();
+        initGoogleMap(savedInstanceState);
+
+        return view;
+    } */
+
+    private void initGoogleMap(Bundle savedInstanceState){
+        // *** IMPORTANT ***
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        // objects or sub-Bundles.
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        mMapView.onCreate(mapViewBundle);
+
+        mMapView.getMapAsync(this);
+    }
+
+    private void initUserListRecyclerView() {
+        //mUserRecyclerAdapter = new UserRecyclerAdapter(mUserList);
+        //UserListRecyclerView.setAdapter(mUserRecyclerAdapter);
+        //mUserListRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        map.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
